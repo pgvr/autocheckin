d723697c4a0f8c43ab9e0bf270d3e60f78996565
pgvr <patrick@productlane.com>
2024-04-21 19:40:28 +0200

    add icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ import { api } from "~/utils/api";
 import { Toaster } from "~/components/ui/sonner";
 
 import { seoConfig } from "~/seo.config";
+import Head from "next/head";
 
 const calFont = localFont({
   src: "../assets/CalSans-SemiBold.woff2",
@@ -34,6 +35,38 @@ const MyApp: AppType<{ session: Session | null }> = ({
             --font-inter: ${interFont.style.fontFamily};
           }
         `}</style>
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/icons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/icons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/icons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/icons/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/icons/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <link rel="shortcut icon" href="/icons/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta
+            name="msapplication-config"
+            content="/icons/browserconfig.xml"
+          />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
 
         <main className={`min-h-screen font-sans`}>
           <DefaultSeo {...seoConfig} />
