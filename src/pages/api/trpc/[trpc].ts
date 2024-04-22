37bb43ef597d4dbede3cfa80baea054d44f1e378
pgvr <patrick@productlane.com>
2024-04-22 18:16:34 +0200

    add trpc error handler

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,6 +1,6 @@
+import { captureException } from "@sentry/nextjs";
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 
-import { env } from "~/env";
 import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
 
@@ -8,12 +8,8 @@ import { createTRPCContext } from "~/server/api/trpc";
 export default createNextApiHandler({
   router: appRouter,
   createContext: createTRPCContext,
-  onError:
-    env.NODE_ENV === "development"
-      ? ({ path, error }) => {
-          console.error(
-            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
-          );
-        }
-      : undefined,
+  onError: ({ path, error }) => {
+    console.error(`❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`);
+    captureException(error);
+  },
 });
